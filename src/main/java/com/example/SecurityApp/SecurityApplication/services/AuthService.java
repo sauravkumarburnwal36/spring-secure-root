7e3136915c3f5c0a16a91fe7929adc9d1863a628
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-12-26 18:22:55 +0530

    Added Session Management using JWT

--- src/main/java/com/example/SecurityApp/SecurityApplication/services/AuthService.java
+++ src/main/java/com/example/SecurityApp/SecurityApplication/services/AuthService.java
@@ -15,6 +15,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserService userService;
+    private final SessionService sessionService;
     public LoginResponseDTO login(LogInDTO logInDTO) {
         Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(logInDTO.getEmail(),logInDTO.getPassword())
@@ -22,6 +23,7 @@ public class AuthService {
         User user=(User)authentication.getPrincipal();
         String accessToken=jwtService.generateAccessToken(user);
         String refreshToken= jwtService.generateRefreshToken(user);
+        sessionService.generateNewSessions(user,refreshToken);
         return new LoginResponseDTO(user.getId(),accessToken,refreshToken);
     }
 
@@ -39,6 +41,7 @@ public class AuthService {
 
     public LoginResponseDTO refresh(String refreshToken) {
         Long userId= jwtService.getUserIdFromToken(refreshToken);
+        sessionService.validateSession(refreshToken);
         User user=userService.getUserById(userId);
         String accessToken= jwtService.generateAccessToken(user);
         return new LoginResponseDTO(user.getId(),accessToken,refreshToken);
